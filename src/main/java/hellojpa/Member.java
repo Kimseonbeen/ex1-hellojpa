e0678cf35b0969b75202ae2a0f49f1a23d539f4c
Kimseonbeen <ksb9242@gmail.com>
2023-04-25 23:15:44 +0900

    @Entity

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,14 +1,22 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * @Entity 사용 시
+ * JPA가 관리
+ * @Entity(name = defalut = class name
+ */
 @Entity     // jpa를 사용확인
 public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
 
     public Member() {}
